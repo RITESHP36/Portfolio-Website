716772ffa3d0881fe0283d04b52b7cece277aa37
RITESHP36 <codingritesh36@gmail.com>
2024-07-18 19:10:21 +0530

    google analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,23 +5,50 @@ import Head from "next/head";
 import Footer from "@/components/Footer";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-	const router= useRouter()
+	const router = useRouter();
 	return (
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon_io/apple-touch-icon.png"/>
-				<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_io//favicon-32x32.png"/>
-				<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_io//favicon-16x16.png"/>
-				<link rel="manifest" href="/images/favicon_io//site.webmanifest"/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/images/favicon_io/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/images/favicon_io//favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/images/favicon_io//favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/images/favicon_io//site.webmanifest" />
 			</Head>
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-9HC9M72TFR"
+			></Script>
+			<Script>
+				{`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9HC9M72TFR');
+          `}
+			</Script>
 			<main className="font-mont bg-light dark:bg-dark w-full min-h-screen">
 				<Navbar />
 				<AnimatePresence mode="wait">
-				<Component key={router.asPath} {...pageProps} />
+					<Component key={router.asPath} {...pageProps} />
 				</AnimatePresence>
 				<Footer />
 				<Analytics />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,34 +1,49 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
-          <style>{`
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+						rel="stylesheet"
+					/>
+					<style>{`
             :root {
               --font-mont: 'Montserrat', sans-serif;
             }
           `}</style>
-        </Head>
-        <body>
-          <Script id="theme-switcher" strategy='beforeInteractive'>
-            {`
+				</Head>
+				<Script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-9HC9M72TFR"
+				></Script>
+				<Script>
+					{`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9HC9M72TFR');
+          `}
+				</Script>
+				<body>
+					<Script id="theme-switcher" strategy="beforeInteractive">
+						{`
               if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                 document.documentElement.classList.add('dark')
               } else {
                 document.documentElement.classList.remove('dark')
               }
             `}
-          </Script>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+					</Script>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
-export default MyDocument
+export default MyDocument;
